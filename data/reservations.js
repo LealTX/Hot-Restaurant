c99663004da160d74383f07887f883f3f92359fd
mig9tx <migue.trujillo@utexas.edu>
2018-11-07 20:15:47 -0600

    added reservations.js

---
+++ data/reservations.js
@@ -0,0 +1,10 @@
+var reservations = [
+    {
+        Customername: "Mario",
+        phoneNumber: "555-5555",
+        customerEmail: "mario@gmail.com",
+        customerID: "44802"
+    }
+];
+
+module.export = reservations;
